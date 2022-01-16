8285a266c6c62c5321ccb17196f2c728c7f24521
Jhoe73 <Jhoenascimento73@gmail.com>
2022-01-16 03:52:18 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+function Form() {
+
+    return (
+        <h1>Form</h1>
+    )
+
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+function Listing() {
+
+    return (
+        <h1>Listing</h1>
+    )
+
+}
+
+export default Listing;
