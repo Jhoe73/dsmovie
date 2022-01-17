1d36278826c542d560ea1b0247b59369872a17fb
Jhoe73 <Jhoenascimento73@gmail.com>
2022-01-17 00:17:28 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -29,9 +29,13 @@ function Listing() {
             });
     }, [pageNumber]);
 
+    const handlePageChange = (newNumber: number) => {
+        setPageNumber(newNumber);
+    }
+
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
 
             <div className="container">
                 <div className="row">
